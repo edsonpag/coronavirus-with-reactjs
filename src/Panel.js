c1daeca02de69617d47b1b0b37f32ce41284c8d3
edsonpag <edsonsilvapagani@gmail.com>
2020-05-06 09:37:46 -0300

    fixed bugs

--- src/Panel.js
+++ src/Panel.js
@@ -35,6 +35,14 @@ function Panel({ data: { confirmed, deaths, recovered, lastUpdate } }) {
                     </h1>
                     <span>Obitos Confirmados</span>
                 </div>
+                {recovered > 0 ? (
+                    <div className="recovered">
+                        <h1>
+                            <CountUp start={0} end={recovered} duration={3} separator="." />
+                        </h1>
+                        <span>Recuperados</span>
+                    </div> 
+                ) : null }
             </div>
         </div>
 
